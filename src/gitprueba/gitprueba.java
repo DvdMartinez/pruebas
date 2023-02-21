3cebadd0c1c69bdf931ae5927c2e2075ec40c53e
chechan <daw111.2023j@gmail.com>
2023-02-21 20:40:26 +0100

    comentado

---
+++ src/gitprueba/gitprueba.java
@@ -0,0 +1,13 @@
+package gitprueba;
+
+public class gitprueba {
+/*sdasdasdasda
+sadasdadsa
+asdasdasdasd
+asdasdasdsad
+sadasdasdasda
+asdasdadsda*/
+	public static void contar3() {
+		//sdaovoipsdhfvashf`sdfioavhi89ahvio
+	}
+}
